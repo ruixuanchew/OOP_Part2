7c560300ad71dfe4b2e9b4782864efb9ef3b0b20
ngjocelyn <2301930.sit@gmail.com>
2024-03-08 17:17:22 +0800

    Removed Direction and Jump Class. Player can now move up and down

--- desktop/src/playerControllerManager/PlayerControllerManager.java
+++ desktop/src/playerControllerManager/PlayerControllerManager.java
@@ -19,18 +19,17 @@ import inputOutputManager.InputHandler;
 public class PlayerControllerManager {
 	private InputHandler inputHandler;
 	private EntityManager entities;
-	private Direction direction;
-	private Jump jump;
 
 	private final float GROUND_LEVEL = 150;
 	private int screenWidth = Gdx.graphics.getWidth();
 
+	private boolean canJump = true;
+	private final float GRAVITY = -150f; // The acceleration due to gravity (in m/s^2)
+	private static final float JUMP_VELOCITY = 180.01f; // JUMP_VELOCITY = sqrt(-2 * GRAVITY * JUMP_HEIGHT) = 160.01f
+
 	public PlayerControllerManager(EntityManager em) {
 		this.entities = em; // Initialize the entity variable
 		this.inputHandler = new InputHandler(); // Initialize the variable to take in user input
-		direction = new Direction(this);
-		jump = new Jump(this);
-
 	}
 
 	// to retrieve user input from methods
@@ -50,23 +49,75 @@ public class PlayerControllerManager {
 	public void setEntities(EntityManager entities) {
 		this.entities = entities;
 	}
+	// Updates the velocity of the player entities based on the input from the
+	// keyboard.
+	public void move() {
+		for (Entity entity : getEntities().getEntityList()) {
+			if (entity.isPlayer()) {
+				if (getMovement().LeftKey()) {
+
+					entity.getVelocity().x = (-entity.getSpeed());
+
+				} else if (getMovement().DownKey()) {
+
+					entity.getVelocity().y = -entity.getSpeed();
+
+				} else if (getMovement().UpKey()) {
+
+					entity.getVelocity().y = entity.getSpeed();
+
+				} else if (getMovement().RightKey()) {
+					entity.getVelocity().x = entity.getSpeed();
+				} else {
+
+					entity.getVelocity().x = 0;
+					// entity.getVelocity().y = 0;
+				}
+			}
+		}
+	}
+
+	// Makes the player entities jump if the space key is pressed and they are
+	// allowed to jump.
+	public void jump() {
+		for (Entity entity : getEntities().getEntityList()) {
+			if (entity.isPlayer()) {
+				if (getMovement().SpaceKey() && canJump) {
+					entity.getVelocity().y += JUMP_VELOCITY; // adjust JUMP VELOCITY
+					canJump = false;
+				}
+			}
+
+		}
+	}
+
+	// Resets the jump flag, allowing the player to jump again
+	public void resetJump() {
+		canJump = true;
+	}
+
+	// Applies gravity to the player entities, making them fall when they are not
+	// jumping.
+	public void applyGravity() {
+		for (Entity entity : getEntities().getEntityList()) {
+			if (entity.isPlayer()) {
+				entity.getVelocity().y += GRAVITY * Gdx.graphics.getDeltaTime();
+			}
+		}
+	}
+
+
 
 	public void update(float deltaTime) {
-		final String ANSI_RESET = "\u001B[0m";
-		final String ANSI_CYAN = "\u001B[36m";
-		final String ANSI_Green = "\u001B[32m";
-
-		direction.move();
-		jump.jump();
-		jump.applyGravity();
+		move();
+		jump();
+		applyGravity();
 
 		// Check if the player is on the ground
 		for (Entity entity : getEntities().getEntityList()) {
 			if (entity.isPlayer()) {
 				entity.setPosX(entity.getPosX() + entity.getVelocity().x * deltaTime);
 				entity.setPosY(entity.getPosY() + entity.getVelocity().y * deltaTime);
-//				System.out.println(String.format(ANSI_CYAN + "Player is at %.2f,%.2f position" + ANSI_RESET,
-//						entity.getPosX(), entity.getPosY()));
 
 				// Check if the entity is at the left edge of the screen
 				if (entity.getPosX() <= 1) {
@@ -80,12 +131,9 @@ public class PlayerControllerManager {
 				if (entity.getPosY() <= GROUND_LEVEL) {
 					entity.setPosY(GROUND_LEVEL);
 					entity.setVelocity(new Vector2(entity.getVelocity().x, 0));
-					jump.resetJump();
+					resetJump();
 				}
 
-			} else {
-//				System.out.println(String.format(ANSI_Green + "Entity is at %.2f, %.2f position" + ANSI_RESET,
-//						entity.getPosX(), entity.getPosY()));
 			}
 
 		}
@@ -93,8 +141,6 @@ public class PlayerControllerManager {
 	}
 
 	public void dispose() {
-		direction.dispose();
-		jump.dispose();
-	}
+}
 
 }
